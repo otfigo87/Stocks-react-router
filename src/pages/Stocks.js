f48640fc441a70e4519b826ce7ac86614b651153
Otmane Aatik <aaotmane@gmail.com>
2023-03-23 23:23:40 -0400

    bonus

--- src/pages/Stocks.js
+++ src/pages/Stocks.js
@@ -6,13 +6,37 @@ const Stocks = ({ stocks }) => {
   return (
     <>
       <h1> Stocks Dashboard</h1>
-      <ol>
-        {stocks.map((stock) => (
-          <Link to={`/stocks/${stock.symbol}`}>
-            <li>{stock.name}</li>
-          </Link>
-        ))}
-      </ol>
+      <div className="stocks-table">
+        <h2 id="head">Most Active Stocks</h2>
+        <table>
+          <tr>
+            <th>Company Name</th>
+            <th>Price</th>
+            <th>Change</th>
+          </tr>
+
+          {stocks.map((stock, i) => (
+            <tr key={i}>
+              <Link
+                id="stock-link"
+                to={`/stocks/${stock.symbol}`}
+                style={{
+                  color: "inherit",
+                  textDecoration: "none",
+                  fontWeight: "900",
+                  fontStyle: "italic",
+                }}
+              >
+                <td>{stock.name}</td>
+              </Link>
+              <td>{stock.open}</td>
+              <td style={{ color: stock.change > 0 ? "red" : "green" }}>
+                {stock.change}
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </>
   );
 }
